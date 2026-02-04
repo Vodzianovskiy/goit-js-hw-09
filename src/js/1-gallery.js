8b182c38891e94138cd6d7075e58afd24098796f
Ivan <vvodzanovskij@gmail.com>
2026-02-04 02:44:00 +0100

    done

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,77 @@
 console.log('Gallery');
+const images = [
+    {
+        preview: './img/1.jpg',
+        original: './img/1-big.jpg',
+        description: 'Photo 1',
+    },
+    {
+        preview: './img/2.jpg',
+        original: './img/2-big.jpg',
+        description: 'Photo 2',
+    },
+    {
+        preview: './img/3.jpg',
+        original: './img/3-big.jpg',
+        description: 'Photo 3',
+    },
+    {
+        preview: './img/row-2-1.jpg',
+        original: './img/row1-1-big.jpg',
+        description: 'Photo 4',
+    },
+    {
+        preview: './img/row-2-2.jpg',
+        original: './img/row1-2-big.jpg',
+        description: 'Photo 5',
+    },
+    {
+        preview: './img/row-2-3.jpg',
+        original: './img/row1-3-big.jpg',
+        description: 'Photo 6',
+    },
+    {
+        preview: './img/row-3-1.jpg',
+        original: './img/row3-1-big.jpg',
+        description: 'Photo 7',
+    },
+    {
+        preview: './img/row-3-2.jpg',
+        original: './img/row3-2-big.jpg',
+        description: 'Photo 8',
+    },
+    {
+        preview: './img/row-3-3.jpg',
+        original: './img/row3-3-big.jpg',
+        description: 'Photo 9',
+    },
+];
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = images
+    .map(
+        ({ preview, original, description }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `
+    )
+    .join('');
+
+galleryList.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,57 @@
 console.log('Form');
+
+const formData = {
+    email: '',
+    message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageTextarea = form.elements.message;
+
+const STORAGE_KEY = 'feedback-form-state';
+
+function saveToStorage() {
+    formData.email = emailInput.value.trim();
+    formData.message = messageTextarea.value.trim();
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function loadFromStorage() {
+    const saved = localStorage.getItem(STORAGE_KEY);
+    if (!saved) return;
+
+    const parsed = JSON.parse(saved);
+
+    formData.email = parsed.email || '';
+    formData.message = parsed.message || '';
+
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+}
+
+loadFromStorage();
+
+form.addEventListener('input', () => {
+    saveToStorage();
+});
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    formData.email = emailInput.value.trim();
+    formData.message = messageTextarea.value.trim();
+
+    if (!formData.email || !formData.message) {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+    formData.email = '';
+    formData.message = '';
+    form.reset();
+});
